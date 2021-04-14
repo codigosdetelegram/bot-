04c658908d5834397893a474a885d7c35ad29749
codigosdetelegram <82538751+codigosdetelegram@users.noreply.github.com>
2021-04-14 22:25:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ https = requests.get("https://www.proxyscan.io/api/proxy?limit=15&type=https&for
 socks4= requests.get("https://www.proxyscan.io/api/proxy?limit=15&type=socks4&format=txt")
 socks5 = requests.get("https://www.proxyscan.io/api/proxy?limit=15&type=socks5&format=txt")
 r = requests.post('https://api.telegram.org/bot'+TOKEN+'/sendMessage', data={'chat_id': CID, 'text': "Los proxys se actualizaran cada 5 minutos"}) 
-print (r.text)
+
 
 open('status.json', 'wb').write(r.content)
 f = open('status.json')
@@ -41,9 +41,6 @@ bot.send_document(CID, file)
 file = open('socks4.txt', 'rb')
 bot.send_document(CID, file)
 
-
-
-print (last_message)
 try:
 	bot.delete_message(CID, last_message -1)
 	bot.delete_message(CID, last_message -2)
