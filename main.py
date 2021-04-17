df6edd911eae45946b1f5d7c0b27105418d3896b
codigosdetelegram <82538751+codigosdetelegram@users.noreply.github.com>
2021-04-17 12:58:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import telebot
 import json
 import os
 
-# TOKEN = os.environ['TOKEN']
-# CID = os.environ['CID']
+TOKEN = os.environ['TOKEN']
+CID = os.environ['CID']
 
 
-#bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
 http = requests.get("https://www.proxyscan.io/api/proxy?limit=15&type=http&format=txt")
 https = requests.get("https://www.proxyscan.io/api/proxy?limit=15&type=https&format=txt")
@@ -38,37 +38,37 @@ while contador != 3:
 	contador = contador +1 
 
 
-# r = requests.post('https://api.telegram.org/bot'+TOKEN+'/sendMessage', data={'chat_id': CID, 'text': "Los proxys se actualizaran cada 5 minutos"}) 
-# open('status.json', 'wb').write(r.content)
-# f = open('status.json')
-# json_file = json.load(f)
-# json_str = json.dumps(json_file)
-# resp = json.loads(json_str)
+r = requests.post('https://api.telegram.org/bot'+TOKEN+'/sendMessage', data={'chat_id': CID, 'text': "Los proxys se actualizaran cada 5 minutos"}) 
+open('status.json', 'wb').write(r.content)
+f = open('status.json')
+json_file = json.load(f)
+json_str = json.dumps(json_file)
+resp = json.loads(json_str)
 
-# last_message = resp['result']['message_id']
+last_message = resp['result']['message_id']
 
 
 
-# file = open('http.txt', 'rb')
-# bot.send_document(CID, file)
+file = open('http.txt', 'rb')
+bot.send_document(CID, file)
 
-# file = open('https.txt', 'rb')
-# bot.send_document(CID, file)
+file = open('https.txt', 'rb')
+bot.send_document(CID, file)
 
-# file = open('socks5.txt', 'rb')
-# bot.send_document(CID, file)
+file = open('socks5.txt', 'rb')
+bot.send_document(CID, file)
 
-# file = open('socks4.txt', 'rb')
-# bot.send_document(CID, file)
+file = open('socks4.txt', 'rb')
+bot.send_document(CID, file)
 
-# try:
-# 	bot.delete_message(CID, last_message -1)
-# 	bot.delete_message(CID, last_message -2)
-# 	bot.delete_message(CID, last_message -3)
-# 	bot.delete_message(CID, last_message -4)
-# 	bot.delete_message(CID, last_message -5)
-# except:
-# 	print("NO HAY NADA QUE BORRAR")
+try:
+	bot.delete_message(CID, last_message -1)
+	bot.delete_message(CID, last_message -2)
+	bot.delete_message(CID, last_message -3)
+	bot.delete_message(CID, last_message -4)
+	bot.delete_message(CID, last_message -5)
+except:
+	print("NO HAY NADA QUE BORRAR")
 
 	
-# print("FIN")
+print("FIN")
